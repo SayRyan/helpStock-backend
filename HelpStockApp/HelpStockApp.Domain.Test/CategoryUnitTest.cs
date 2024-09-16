5f6a301bc43c0c4399414599700c49329203b774
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-09-16 11:46:55 -0300

    Resultados de Testes.

---
+++ HelpStockApp/HelpStockApp.Domain.Test/CategoryUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using HelpStockApp.Domain.Entities;
+using FluentAssertions;
+using Xunit;
+using HelpStockApp.Domain.Validation;
+
+namespace HelpStockApp.Domain.Test
+{
+        #region Testes Positivos de Categoria
+    public class CategoryUnitTest
+    {
+        [Fact(DisplayName = "Create Category With Valid State")]
+        public void CreateCategory_WithValidParemeters_ResultObejectsValidState()
+        {
+            Action action= () => new Category(1, "Category name");
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+        #endregion
+
+        #region Testes Negativos de Categoria
+
+        [Fact(DisplayName = "Create Category With Invalid Id")]
+        public void CreateCategory_WithInvalidParemeters_ResultException()
+        {
+            Action action = () => new Category(-1, "Eletronics");
+            action.Should().Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid Id Value.");
+        }
+        /*
+            Create Category With Name Too Short Parameter
+            Create Category With Name Null Parameter
+            Create Category With Name Missing Parameter
+            Create Category With Name Invalid Id Caracter Id
+            Create Category With Name Parameter Name Alone
+         */
+        #endregion
+    }
+}

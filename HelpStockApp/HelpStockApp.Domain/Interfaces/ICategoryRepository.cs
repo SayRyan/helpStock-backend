9c2a0f60d0a947651d4a08f2c4b0507fc87d91e7
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-10-21 10:23:04 -0300

    Aula1021 - Interfaces de Domain

---
+++ HelpStockApp/HelpStockApp.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using HelpStockApp.Domain.Entities;
+
+namespace HelpStockApp.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+
+        Task<Category> GetById(int? id);
+
+        Task<Category> CreateCategory(Category category);
+
+        Task<Category> UpdateCategory(Category category);
+
+        Task<Category> RemoveCategory(Category category);
+    }
+}

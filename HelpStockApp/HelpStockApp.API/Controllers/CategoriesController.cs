8b03f8afd6aadd2ea4b6e99f8c9d3ffc5049b91a
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-11-19 14:37:56 -0300

    Crud - Categories

--- HelpStockApp/HelpStockApp.API/Controllers/CategoriesController.cs
+++ HelpStockApp/HelpStockApp.API/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using HelpStockApp.Application.DTOs;
 using HelpStockApp.Application.Interfaces;
+using HelpStockApp.Domain.Entities;
 
 namespace HelpStockApp.API.Controllers
 {
@@ -35,6 +36,45 @@ namespace HelpStockApp.API.Controllers
             {
                 return NotFound("Category not found");
             }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] CategoryDTO categoryDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            await _categoryService.Add(categoryDto);
+            return CreatedAtAction(nameof(Get), new { id = categoryDto.Id }, categoryDto);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(int id, [FromBody] CategoryDTO categoryDto)
+        {
+            if (id != categoryDto.Id)
+            {
+                return BadRequest("Id not Found.");
+            }
+
+            if (categoryDto == null)
+            {
+                return BadRequest("Id not Found.");
+            }
+
+            await _categoryService.Update(categoryDto);
+            return Ok(categoryDto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Delete(int id)
+        {
+            var category = await _categoryService.GetCategoryById(id);
+            if (category == null)
+            {
+                return NotFound("The category delete not found");
+            }
+
+            await _categoryService.Remove(id);
             return Ok(category);
         }
     }
